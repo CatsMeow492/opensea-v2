f90c44b93473cdbf237123ce3593a617ae4ae2ca
Taylor Mohney <taylormohneytech@gmail.com>
2022-09-20 16:18:23 -0700

    header updates

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import openSeaLogo from '../assets/opensea.png'
+import { Link } from 'react-router-dom'
+import Image from 'next/image'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { CgProfile } from 'react-icons/cg'
+import {MdOutlineAccountBalanceWallet} from 'react-icons/md'
 
 const style = {
     wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
@@ -11,12 +17,21 @@ const style = {
     headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
     headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
   }
-
-  import React from 'react'
   
   function Header() {
     return (
-      <div>Header</div>
+      <div className={style.wrapper}>
+        <Link href="/">
+          <div className={style.logoContainer}>
+            <Image src={openSeaLogo} height={40} width={40} />
+            <div className={style.logoText}>Opensea</div>
+          </div>
+        </Link>
+        <div className={style.searchBar}>
+          <div className={style.searchIcon}>
+            🔍</div>
+        </div>
+      </div>
     )
   }
   
